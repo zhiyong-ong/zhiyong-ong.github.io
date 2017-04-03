43f362e6696d604f2100aba179e14ca2398327cc
Zhi Yong <zhiyong.ong@hotmail.com>
2017-04-04 02:06:29 +0800

    add validation

--- js/custom.js
+++ js/custom.js
@@ -12,49 +12,6 @@ $(document).ready(function() {
     initSmoothScroll();
     initSticky();
     initWaypoints();
-
-    $('#contactForm')
-        .formValidation({
-            framework: 'bootstrap',
-            icon: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
-            },
-            fields: {
-                name: {
-                    row: '.col-xs-4',
-                    validators: {
-                        notEmpty: {
-                            message: 'The name is required'
-                        }
-                    }
-                },
-               
-                _replyto: {
-                    validators: {
-                        notEmpty: {
-                            message: 'The email address is required'
-                        },
-                        emailAddress: {
-                            message: 'The input is not a valid email address'
-                        }
-                    }
-                },
-                message: {
-                    validators: {
-                        notEmpty: {
-                            message: 'The message is required'
-                        },
-                        stringLength: {
-                            max: 700,
-                            message: 'The message must be less than 700 characters long'
-                        }
-                    }
-                },
-                
-            }
-        })
     
 });
 
